8d52a05eb05d2f5e4dfaa45cd4ac7322738f165a
tomtomcrypto <titiomm@gmail.com>
2022-06-16 20:11:41 +0000

    gas limit refactor

--- serializeEVMTransaction.js
+++ serializeEVMTransaction.js
@@ -20,11 +20,10 @@ const evmApi = new TelosEvmApi({
     telosPrivateKeys: []
 });
 (async () => {
-    var evmAccount, linkedAddress, nonce;
     try {
-        evmAccount = await evmApi.telos.getEthAccountByTelosAccount(nativeAccount);
-        linkedAddress = evmAccount.address;
-        nonce = parseInt(await evmApi.telos.getNonce(linkedAddress), 16);
+        var evmAccount = await evmApi.telos.getEthAccountByTelosAccount(nativeAccount);
+        var linkedAddress = evmAccount.address;
+        var nonce = parseInt(await evmApi.telos.getNonce(linkedAddress), 16);
     } catch(e) {
         console.log(e.message);
         return;
@@ -32,9 +31,9 @@ const evmApi = new TelosEvmApi({
 
     const feeData = await provider.getFeeData()
     const gasPrice = BigNumber.from(`0x${await evmApi.telos.getGasPrice()}`)
-    const gasLimit = BigNumber.from(`0x1E8480`);
+    const gasLimit = 26166;
 
-// POPULATE TRANSACTION
+    // POPULATE TRANSACTION
     try {
         var unsignedTrx =  await contract.populateTransaction.setLockDuration(3600);
     } catch(e) {
@@ -53,26 +52,9 @@ const evmApi = new TelosEvmApi({
         return;
     }
     raw = raw.replace(/^0x/, '');
-    
-   var auth = {
-        "threshold": 1,
-        "keys": [],
-        "waits": [],
-        "accounts": [
-            {
-                "permission": {
-                    "actor": "prods.evm",
-                    "permission": "active"
-                },
-                "weight": 1
-            }
-        ]
-    }
-
 
     // PRINT IT OUT
     console.log("SERIALIZED_TX: ", raw);
     console.log("LINKED_ADDRESS: ", linkedAddress.replace(/^0x/, ''));
-    console.log("CLEOS COMMAND: ",  'cleos --url https://testnet.telos.caleos.io/ push action eosio.evm raw \'{"account": "'+nativeAccount+'", "permission": "active", "parent": "", "auth": '+ auth +',  "ram_payer": '+nativeAccount+', "tx": "'+ raw +'" , "estimate_gas": false, "sender": "'+ linkedAddress.replace(/^0x/, '') +'"}\' -p ' + nativeAccount);
 
 })()
