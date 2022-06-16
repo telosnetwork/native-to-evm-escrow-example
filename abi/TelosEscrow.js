da65e44fe3b34f119d710d278056c885286c8c0c
Thomas Cuvillier <titiomm@gmail.com>
2022-06-16 14:33:38 +0200

    Update and rename MintableToken.js to TelosEscrow.js

---
+++ abi/TelosEscrow.js
@@ -0,0 +1,17 @@
+const contractABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_lockDuration",
+                "type": "uint256"
+            }
+        ],
+        "name": "setLockDuration",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+
+export default contractABI;
